2d666674d0ae551a3a12a3a744ae0392f7937ba1
josiascta <104440548+josiascta@users.noreply.github.com>
2022-06-14 15:45:31 -0300

    att2

--- Projeto - Combate/Teste - Combate.py
+++ Projeto - Combate/Teste - Combate.py
@@ -1,21 +1,6 @@
-# by Josias.
 import time
 import random
-'''def txt(texto):
-    for i in range(1):
-        for c in texto:
-            print(c, end='')
-            # time.sleep(0.03)
-        print()
 
-
-txt("=-" * 32)
-print(f"""{'|':<0} {'Bem-Vindo(a) ao [NOME DO JOGO]':^60} {'|'}
-{'|':<0} {'':^60} {'|'}
-{'|':<0} {'Selecione uma classe para começar a jornada!':^60} {'|'}
-{'|':<0} {'':^60} {'|'}
-{'|':<0} {'1-> Guerreiro  2-> Elfo  3-> Mago':^60} {'|'}""")
-txt("=-" * 32)'''
 def dado():
     return random.randint(1,6)
 
@@ -31,4 +16,4 @@ opcoes = int(input(f'''
 Escolha o que você vai fazer: '''))
 
 if opcoes == 1:
-    
+
