4b6cb17cd750383d2fcb0c464b4f8484ed333cf6
Jeff <jefferson.mangueira@academico.ifpb.edu.br>
2022-06-16 15:02:28 -0300

    Acrescentando verificador de escolhas inexistentes.

--- Projeto - Combate/Teste - Combate.py
+++ Projeto - Combate/Teste - Combate.py
@@ -22,6 +22,9 @@ Escolha o que você vai fazer: '''))
 
     if opcoes == 1:
         controle = int(input('Se você quiser jogar o dado para atacar digite (1), se quiser voltar digite (2): '))
+        while 1 != controle != 2:
+            controle = int(input("Para jogar o dado digite (1), para voltar digite (2): "))
+
         if controle == 1:
             print('O dado foi jogado...')
             time.sleep(1)
--- Projeto - História/Teste - História.py
+++ Projeto - História/Teste - História.py
@@ -1,53 +1,47 @@
 # by Jefferson.
-from prettytable import PrettyTable
-####
+####################
 import time
 import random
+####################
 
-personagens = ["Guerreiro", "Arqueiro", "Mago"]
-escolha = []
-lista = [0, 0, 0]  # [HP, Força, Mana]
-
-
+##################################
+## DADO.
 def dado():
     x = random.randint(1, 6)
     return x
+
 
 '''numero = 0
 for i in range(1):
     numero = dado()
     print(numero)'''
 
-
+## Impressão de texto.
 def txt(texto):
     for i in range(1):
         for c in texto:
             print(c, end='')
-            # time.sleep(0.03)
+            time.sleep(0.03)
         print()
 
 
-table = PrettyTable()
-superior = table.field_names = ['Classe', 'Vida', 'Força', 'Mana']
-tabela_guerreiro = table.add_row(["Guerreiro", 100, 110, 90])
-tabela_elfo = table.add_row(["Elfo", 110, 90, 100])
-tabela_mago = table.add_row(["Mago", 90, 100, 110])
-
-txt("=-" * 32)
-print(f"""{'|':<0} {'Bem-Vindo(a) ao [NOME DO JOGO]':^60} {'|'}
-{'|':<0} {'':^60} {'|'}
-{'|':<0} {'Selecione uma classe para começar a jornada!':^60} {'|'}
-{'|':<0} {f'{superior}':^60} {'|'}
-{'|':<0} {f'{}':^60} {'|'}
-{'|':<0} {'1.Guerreiro  2.Elfo  3.Mago':^60} {'|'}""")
-txt("=-" * 32)
-escolha.append(int(input("Qual classe deseja? ")))
+def quadrado_historia(t):
+    txt("=-" * 32)
+    print(f"""{'|':<0} {'Bem-Vindo(a) ao [NOME DO JOGO]':^60} {'|'}
+    {'|'} {'':^60} {'|'}
+    {'|'} {'Selecione uma classe para começar a jornada!':^60} {'|'}
+    {'|'} {f'':^60} {'|'}
+    {'|'} {'1.Guerreiro  2.Elfo  3.Mago':^60} {'|'}
+    {'|'} {'|':^20}{f'':^20}{'|':^20} {'|'}""")
+    txt("=-" * 32)
+    escolha.append(int(input("Qual classe deseja? ")))
 
 
-table.field_names = ['Classe', 'Vida', 'Força', 'Mana']
-table.add_row(["Guerreiro", 100, 110, 90])
-table.add_row(["Elfo", 110, 90, 100])
-table.add_row(["Mago", 90, 100, 110])
+##################################
 
-print(table)
+personagens = ["Guerreiro", "Arqueiro", "Mago"]
+escolha = []
+lista = [0, 0, 0]  # [HP, Força, Mana]
 
+
+quadrado_historia()
