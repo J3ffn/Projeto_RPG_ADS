13e5968e03c04272ae1475bb8a12c61a4c1b1cd2
josiascta <104440548+josiascta@users.noreply.github.com>
2022-06-17 23:33:14 -0300

    Update Teste - Combate.py

--- Projeto - Combate/Teste - Combate.py
+++ Projeto - Combate/Teste - Combate.py
@@ -3,11 +3,11 @@ import time, random
 def dado():
     return random.randint(1, 6)
 
-itens = [['Poção Pequena', 'Poção Média', 'Poção Grande'],[4, 5, 6]]
+itens = [['Poção Pequena', 'Poção Média', 'Poção Grande'],[0, 0, 0]]
 atributos = [90, 100, 110]
 inimigo1 = [100, 100, 0]
 atributosbatalha = atributos[:]
-
+opcoes = 0
 while inimigo1[0] > 0:
     opcoes = int(input(f'''{'-' * 40}
     1-> Ataque      2-> Especial      
@@ -17,6 +17,7 @@ while inimigo1[0] > 0:
 Escolha o que você vai fazer: '''))
 
 if opcoes == 1:
-        controle = int(input('Se você quiser jogar o dado para atacar digite (1), se quiser voltar digite (2): '))
-        while 1 != controle != 2:
-            controle = int(input("Para jogar o dado digite (1), para voltar digite (2): "))
+    controle = int(input('Se você quiser jogar o dado para atacar digite (1), se quiser voltar digite (2): '))
+    while 1 != controle != 2:
+        controle = int(input("Para jogar o dado digite (1), para voltar digite (2): "))
+        
